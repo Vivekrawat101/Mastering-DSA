c7753e7cde4d8fbb7cec1afd4358acb253eaf306
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2025-07-08 22:21:49 +0530

    added solve for day 1 questions

--- Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day1_Basics_O_n/code/04_suffix_array.py
+++ Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day1_Basics_O_n/code/04_suffix_array.py
@@ -1,3 +1,19 @@
 # Problem: Compute suffix sum array.
 # Input: [1, 2, 3]
 # Output: [6, 5, 3]
+
+
+def suffix_sum_array(arr):
+    
+    n = len(arr)
+    ctr = arr[n-1]
+    
+    for i in range(n-2, -1, -1):
+        
+        ctr = ctr + arr[i]
+        
+        arr[i] = ctr 
+    
+    return arr
+
+print(suffix_sum_array([1,2,3,4,5]))
