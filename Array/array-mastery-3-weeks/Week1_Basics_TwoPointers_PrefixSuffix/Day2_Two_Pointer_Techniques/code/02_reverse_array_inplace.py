e78d96bf93626cd12f280546f3de9be2e40ff9ee
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2025-07-09 23:26:49 +0530

    add reverse array using 2 pointers

--- Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day2_Two_Pointer_Techniques/code/02_reverse_array_inplace.py
+++ Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day2_Two_Pointer_Techniques/code/02_reverse_array_inplace.py
@@ -2,3 +2,23 @@
 #
 # Input: [1, 2, 3]
 # Output: [3, 2, 1]
+
+
+def reverse_array(arr):
+    
+    n = len(arr)
+    i = 0
+    j = n-1
+    
+    
+    while i < (n//2):   #i<j : standard condition but this also works fine :)
+
+        arr[i], arr[j] = arr[j], arr[i]
+        
+        i+=1
+        j-=1
+    
+    return arr
+        
+
+print(reverse_array([1,2,3,4]))           
