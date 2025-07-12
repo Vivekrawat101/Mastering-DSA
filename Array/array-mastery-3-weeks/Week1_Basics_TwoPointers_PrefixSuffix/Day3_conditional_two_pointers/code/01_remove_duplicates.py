e4a89b1e61b25179736e2429e156d412812f9f5b
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2025-07-12 20:24:57 +0530

    add leetcode solve

--- Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day3_conditional_two_pointers/code/01_remove_duplicates.py
+++ Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day3_conditional_two_pointers/code/01_remove_duplicates.py
@@ -29,9 +29,27 @@ def remove_duplicates(arr):
             
         j+=1
 
-    return i    
+    return i
+
+
+def leetcode_solve(arr):
+    
+    i = 0
+    
+    for j in range(1, len(arr)):
+        
+        if arr[j] != arr[i]:
+            # if iter is not equals to slow pointer then distinct element is found 
+            # keep it at next slow pointer index
+            i += 1
+            arr[i] = arr[j]
+    
+    return i+1
+            
+  
             
 arr=[1,1,1,2,2,3,3,3,4,4,4,5]
 
-k = remove_duplicates(arr)
+# k = remove_duplicates(arr)
+k = leetcode_solve(arr)
 print(f'{k}, nums = {arr[:k]}')
