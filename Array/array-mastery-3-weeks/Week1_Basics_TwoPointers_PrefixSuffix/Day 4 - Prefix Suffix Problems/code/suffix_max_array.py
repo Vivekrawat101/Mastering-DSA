87decf68b543abf044879aed9e0cea7c06b855c4
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2025-07-17 02:23:01 +0530

    add suffix Max Array

--- Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day 4 - Prefix Suffix Problems/code/suffix_max_array.py
+++ Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day 4 - Prefix Suffix Problems/code/suffix_max_array.py
@@ -10,3 +10,37 @@ Output: 2  # 5 and 1
 """
 
 
+def suffix_max(arr):
+
+    suffix = arr[:]
+    right_max = suffix[-1]
+    
+    for idx in range(len(suffix)-2,-1,-1):    
+        
+        if suffix[idx] < right_max:
+            suffix[idx] = right_max
+        
+        else:
+            right_max = suffix[idx]
+        
+    
+    return suffix
+    
+arr=[4, 3, 2, 5, 1]
+suffix_arr = suffix_max(arr)
+print(suffix_arr)
+n = len(arr)
+max_num = arr[-1]
+count = 0
+
+    
+for idx in range(n-2, -1, -1):
+
+    if arr[idx] > max_num:
+        
+        count += 1
+        max_num = arr[idx]
+        
+    
+print(count)
+     
