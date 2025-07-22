7d2ded01c3d08550eeba8622d88c9c318456e3bf
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2025-07-22 23:41:09 +0530

    add subarray sum equals k solve

--- Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day 5 - Prefix + Two Pointers/code/subarray_sum_equals_k.py
+++ Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day 5 - Prefix + Two Pointers/code/subarray_sum_equals_k.py
@@ -10,3 +10,22 @@ Output: 2  # Subarrays: [1,1] from index 0-1 and 1-2
 
 # Write your implementation here
 
+def solution(nums,k):
+    left = 0
+    curr_sum = 0
+    count = 0
+    
+    for right in range(len(nums)):
+        curr_sum += nums[right]
+        
+        while curr_sum > k:
+            curr_sum -= nums[left]
+            left += 1
+        
+        if curr_sum == k:
+            count += 1
+    
+    return count
+
+print(solution([1, 2, 1, 2, 1], 3))
+    
