d7e4ad1f4e996da258e72553f0028e95d28884aa
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2025-07-27 23:40:40 +0530

    add day 7

--- Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day 7 - Mock Test and Recap/code/count_subarrays_sum_range.py
+++ Array/array-mastery-3-weeks/Week1_Basics_TwoPointers_PrefixSuffix/Day 7 - Mock Test and Recap/code/count_subarrays_sum_range.py
@@ -9,3 +9,23 @@ Output: 3  # [1,2], [2,3], [3]
 """
 
 # Write your implementation here
+def solution(arr, L, R):
+    
+    count = 0
+    s = {0:1}
+    curr_sum = 0
+    
+    for num in range(0, len(arr)):
+        
+        curr_sum += arr[num]
+        
+        for key in range(curr_sum-R, curr_sum-L +1) :
+            
+            if key in s:
+                
+                count += s[key]
+
+        s[curr_sum] = s.get(curr_sum,0)+1
+    return count
+
+print(solution([1,2,3],3,5))
